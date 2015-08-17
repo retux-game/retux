3f7984fe8b6436ef5c00563321982a6571e3f1e6
onpon4 <onpon4@riseup.net>
2015-08-17 09:27:54 -0400

    Bumped version number

--- retux.py
+++ retux.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.1a1"
+__version__ = "0.1a2"
 
 import argparse
 import json
