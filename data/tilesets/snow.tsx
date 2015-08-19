ff8871c0fc88f5ef234528f169cd5ba44fece5c8
onpon4 <onpon4@riseup.net>
2015-08-19 13:31:05 -0400

    Pulled newer graphics from upstream.

--- data/tilesets/snow.tsx
+++ data/tilesets/snow.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="snow" tilewidth="32" tileheight="32">
- <image source="../images/tiles/tileset_snow.png" width="224" height="160"/>
+ <image source="../images/tiles/tileset_snow.png" width="448" height="544"/>
 </tileset>
