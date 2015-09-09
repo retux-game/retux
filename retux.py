18f642aa1b56432620952a38313fdf8f25da511b
onpon4 <onpon4@riseup.net>
2015-09-09 14:53:26 -0400

    ReTux 0.1.

--- retux.py
+++ retux.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.1a7"
+__version__ = "0.1"
 
 import argparse
 import datetime
