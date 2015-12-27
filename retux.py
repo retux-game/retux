463f116313b4e811c8c6740acf5cbbaa6631a50c
onpon4 <onpon4@riseup.net>
2015-12-26 19:47:59 -0500

    Got rid of the whole auto-throwing thing with rocks.
    This is mainly because of the rock-skating glitch; I don't mind that
    glitch being there, but this will prevent people from just casually
    accidentally discovering it, which could be rather annoying (it could
    easily launch you into a pit or into a group of enemies).

--- retux.py
+++ retux.py
@@ -4530,8 +4530,6 @@ class Rock(FallingObject, WinPuffObject, xsge_physics.MobileColliderWall,
             self.tangible = False
             self.xvelocity = 0
             self.yvelocity = 0
-            if other.action_pressed:
-                other.action()
 
     def stop_left(self):
         self.xvelocity = 0
