e4acc10d27ed63ba261ec70e8490f0aec3d1efcb
onpon4 <onpon4@riseup.net>
2016-04-05 09:39:33 -0400

    Version 0.5 release.

--- retux.py
+++ retux.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.5a0"
+__version__ = "0.5"
 
 import argparse
 import datetime
