45e3e425ec4b5a30bf78c1dc168848186386c069
onpon4 <onpon4@riseup.net>
2016-10-20 20:27:00 -0400

    Fixed malformed credits.json. 1.2.1 release.
    How embarassing...

--- retux.py
+++ retux.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "1.2"
+__version__ = "1.2.1"
 
 import argparse
 import datetime
