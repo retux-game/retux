a7b226c40b3d16bba369ac214d61d0cc5528cdb0
Jorge Maldonado Ventura <jorgesumle@freakspot.net>
2017-04-12 18:30:50 +0200

    Added --version option

--- retux.py
+++ retux.py
@@ -78,7 +78,10 @@ if six.PY2:
 else:
     gettext.install("retux", os.path.abspath(os.path.join(dirs[0], "locale")))
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog="ReTux")
+parser.add_argument(
+    "--version", action="version", version="%(prog)s " + __version__,
+    help=_("Output version information and exit"))
 parser.add_argument(
     "-p", "--print-errors",
     help=_("Print errors directly to stdout rather than saving them in a file."),
