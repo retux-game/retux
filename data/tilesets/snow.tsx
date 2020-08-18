b693d39ca62dbf9f7e8f87340a884e1fb0b17ba1
Layla Marchant <diligentcircle@riseup.net>
2020-08-18 10:26:43 -0400

    Revert "stuffs"
    This reverts commit dfd39b648af4818994a128aef744318ce25b9281.

--- data/tilesets/snow.tsx
+++ data/tilesets/snow.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="snow" tilewidth="32" tileheight="32" tilecount="392" columns="14">
+<tileset version="1.2" tiledversion="1.3.4" name="tileset_snow" tilewidth="32" tileheight="32" tilecount="392" columns="14">
  <image source="../images/tiles/tileset_snow.png" width="448" height="896"/>
 </tileset>
